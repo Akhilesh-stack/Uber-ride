06bd85d7f8faaac14ccc0958e5b21f52b75aca00
Akhilesh Kumar Yadav <akhildclewis3245@gmail.com>
2026-05-11 20:38:07 +0530

    my third commit

--- uber-video-main/Backend/db/db.js
+++ uber-video-main/Backend/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 function connectToDb() {
-    mongoose.connect(process.env.DB_CONNECT
+    mongoose.connect(process.env.MONGO_URI
     ).then(() => {
         console.log('Connected to DB');
     }).catch(err => console.log(err));
@@ -10,3 +10,7 @@ function connectToDb() {
 
 
 module.exports = connectToDb;
+
+// PORT=5000
+// MONGO_URI=mongodb+srv://akyadav961682_db_user:Akhil123@cluster0.k5nbiem.mongodb.net/uber
+// JWT_SECRET=fdf8e20e0509c1a30290b32735d630ca6dc9b61720b48a5bb9dd9dba27f807c80a2e8ac7aca2e610ed0732adf654bdff4309a9e1197f1ccebe48e137b8751bb0
--- uber-video-main/Backend/server.js
+++ uber-video-main/Backend/server.js
@@ -1,9 +1,13 @@
+require('dotenv').config()
 const http = require('http');
 const app = require('./app');
 const { initializeSocket } = require('./socket');
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
+
+const cors= require('cors');
+app.use(cors());
 
 initializeSocket(server);
 
